e2d30f47c539f397b198471b258d8f41db8a8235
gamertoky1188gro <tokyintelligentgamer@gmail.com>
2026-03-27 00:24:19 +0600

    meow

--- server/utils/db.js
+++ server/utils/db.js
@@ -4,6 +4,16 @@ export async function ensureDatabaseConnection() {
   if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is required to start the server (PostgreSQL)')
   }
+  try {
+    const safeUrl = new URL(process.env.DATABASE_URL)
+    const maskedPass = safeUrl.password ? '***' : ''
+    const safe = `${safeUrl.protocol}//${safeUrl.username}${maskedPass ? `:${maskedPass}` : ''}@${safeUrl.host}${safeUrl.pathname}`
+    // eslint-disable-next-line no-console
+    console.log('[db] Using DATABASE_URL:', safe)
+  } catch {
+    // eslint-disable-next-line no-console
+    console.log('[db] Using DATABASE_URL (unparsed):', '[redacted]')
+  }
   await prisma.$connect()
 }
 
