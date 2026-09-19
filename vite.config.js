54e52e7dec458715f34a3735a1fccf2ba6d400d9
Tokyi <tokyi@hartonhub.com>
2026-09-19 20:06:51 +0600

    enable production source maps for debugging

--- vite.config.js
+++ vite.config.js
@@ -48,7 +48,7 @@ export default defineConfig({
 			},
 		},
 		reportCompressedSize: false,
-		sourcemap: process.env.NODE_ENV !== "production",
+		sourcemap: true,
 	},
 	plugins: [tailwindcss(), react()],
 	server: {
